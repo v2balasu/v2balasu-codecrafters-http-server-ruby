22963cf0945e34b7551f497ae94674602fd1760c
v2balasu <vithuranb@gmail.com>
2024-12-07 18:08:08 -0500

    extract url

--- app/server.rb
+++ app/server.rb
@@ -6,18 +6,31 @@ print('Logs from your program will appear here!')
 server = TCPServer.new('localhost', 4221)
 
 HTTP_METHOD_REGEX = /^(GET|POST|PUT|PATCH|DELETE|OPTIONS|HEAD|TRACE|CONNECT)/.freeze
+HTTP_RESPONSE_OK = "HTTP/1.1 200 OK\r\n\r\n".freeze
+HTTP_RESPONSE_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n".freeze
 
 def start_connection(client_socket)
   # TODO: Timeout
   loop do
     req = client_socket.gets
-    client_socket.puts("HTTP/1.1 200 OK\r\n\r\n") if req&.match(HTTP_METHOD_REGEX)
+
+    if req&.match(HTTP_METHOD_REGEX)
+      res = process_request(req)
+      client_socket.puts(res)
+    end
   rescue StandardError => e
     puts "Error reading from client socket #{e.message}"
     break
   end
 
   client_socket.close
+end
+
+def process_request(req)
+  req_method, path = req.split("\s")
+  return HTTP_RESPONSE_OK unless req_method == 'GET'
+
+  path == '/' ? HTTP_RESPONSE_OK : HTTP_RESPONSE_NOT_FOUND
 end
 
 loop do
